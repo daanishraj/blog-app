abb229b5bc2c63d74599b256a41a78158032e020
Daanish Raj <daanishraj@gmail.com>
2023-11-12 18:31:22 +0100

    Add endpoint for deleting a user and all their blogs

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const userRouter = require('express').Router()
 const User = require('../models/user')
+const Blog = require('../models/blog')
 
 
 userRouter.get('/', async (request, response) => {
@@ -18,6 +19,17 @@ userRouter.post('/', async(request, response) => {
   const user = new User({ username, name, passwordHash })
   const savedUser = await user.save()
   return response.status(201).json(savedUser)
+})
+
+userRouter.delete('/:id', async(request, response) => {
+  const user = await User.findById(request.params.id)
+  if (! user) {
+    return response.status(404).json({ error: 'user could note be found' })
+  }
+  const blogsToDelete = user.blogs.map(blogId => blogId.toString())
+  await Blog.deleteMany({ _id: { $in: blogsToDelete } })
+  await User.findByIdAndRemove(request.params.id)
+  return response.status(204).end()
 })
 
 module.exports = userRouter
